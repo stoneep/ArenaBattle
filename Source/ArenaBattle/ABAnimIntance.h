c914be4461df6a179d4005ed1a67ca2d6fa32b24
KimWooSeok <blu7archive@gmail.com>
2023-09-30 22:44:01 +0900

    initial commit

---
+++ Source/ArenaBattle/ABAnimIntance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimIntance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UABAnimInstance();
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	//void SetPawnSpeed(float NewPawnSpeed) { CurrentPawnSpeed = NewPawnSpeed; }
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn,
+		Meta = (AllowPrivateAccess = true))
+	float CurrentPawnSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool IsInAir;
+};
+
---
+++ Source/ArenaBattle/ArenaBattle.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaBattle.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(ArenaBattle);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
+
+
+ 
