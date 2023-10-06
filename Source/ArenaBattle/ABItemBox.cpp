cdde49be202c1c52f628320cfbef51673cd76092
wooseok <140233680+userwooseok@users.noreply.github.com>
2023-10-06 15:38:04 +0900

    332back

---
+++ Source/ArenaBattle/ABItemBox.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABItemBox.h"
+
+// Sets default values
+AABItemBox::AABItemBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Box = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BOX"));
+
+	RootComponent = Trigger;
+	Box->SetupAttachment(RootComponent);
+
+	Trigger->SetBoxExtent(FVector(40.f, 42.f, 30.f));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BOX(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Environments/Breakables/StaticMesh/Box/SM_Env_Breakables_Box1.SM_Env_Breakables_Box1'"));
+	if (SM_BOX.Succeeded())
+	{
+		Box->SetStaticMesh(SM_BOX.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AABItemBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABItemBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
